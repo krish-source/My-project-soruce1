83d840a024890c084f232bf182d39efe4159bbcf
krish-source <83317611+krish-source@users.noreply.github.com>
2021-08-03 21:04:38 +0530

    Python integration to Power BI and scatter plot viz.
    Loading a DF & displaying a scatter plot.

---
+++ test2.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+df = pd.DataFrame({ 
+    'Fname':['Harry','Sally','Paul','Abe','June','Mike','Tom'], 
+    'Age':[21,34,42,18,24,80,22], 
+    'Weight': [180, 130, 200, 140, 176, 142, 210], 
+    'Gender':['M','F','M','M','F','M','M'], 
+    'State':['Washington','Oregon','California','Washington','Nevada','Texas','Nevada'],
+    'Children':[4,1,2,3,0,2,0],
+    'Pets':[3,2,2,5,0,1,5] 
+}) 
+print (df)
+
+
+#scatter plot viz in Power BI
+
+data = [['Alex',10],['Bob',12],['Clarke',13]]
+df = pd.DataFrame(data,columns=['Name','Age'],dtype=float)
+print (df)
